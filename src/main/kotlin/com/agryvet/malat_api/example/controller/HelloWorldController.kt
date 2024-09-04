48912e7354cbf5b1d972d939f884b92822ab5048
Alexis Guadarrama <alexgsisc@gmail.com>
2024-09-04 15:12:11 -0600

    :sparkles: add language, you add headers:  Accept-Language: en

--- src/main/kotlin/com/agryvet/malat_api/example/controller/HelloWorldController.kt
+++ src/main/kotlin/com/agryvet/malat_api/example/controller/HelloWorldController.kt
@@ -2,16 +2,20 @@ package com.agryvet.malat_api.example.controller
 
 
 import com.agryvet.malat_api.example.model.HelloWorldBean
+import org.springframework.context.MessageSource
+import org.springframework.context.i18n.LocaleContext
+import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
-class HelloWorldController {
+class HelloWorldController(private val messageSource: MessageSource) {
 
     @GetMapping("hello-world")
     fun helloWorld(): String {
-        return  "Hello world spring"
+        return "Hello world spring"
     }
 
     @GetMapping("hello-word-bean")
@@ -20,7 +24,15 @@ class HelloWorldController {
     }
 
     @GetMapping("hello-word-path-variables/{name}")
-    fun helloWorldPathVariables(@PathVariable name:String): HelloWorldBean {
+    fun helloWorldPathVariables(@PathVariable name: String): HelloWorldBean {
         return HelloWorldBean(String.format("Hello world, %s", name))
+    }
+
+    @GetMapping("hello-world-internationalized")
+    fun helloWorldInternationalized(): String {
+        val locale: Locale = LocaleContextHolder.getLocale()
+        println("the located is: $locale")
+        val message = messageSource.getMessage("good.morning.message", null, "Default Message", locale)
+        return message!!
     }
 }
