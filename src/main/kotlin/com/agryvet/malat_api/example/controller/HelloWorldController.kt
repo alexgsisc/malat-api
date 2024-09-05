4842e87e9a17e4bbb00ad3def7ef13db5f59d573
Alexis Guadarrama <alexgsisc@gmail.com>
2024-09-04 18:18:40 -0600

    :sparkles: I have tp remove unused import in controller class

--- src/main/kotlin/com/agryvet/malat_api/example/controller/HelloWorldController.kt
+++ src/main/kotlin/com/agryvet/malat_api/example/controller/HelloWorldController.kt
@@ -3,7 +3,6 @@ package com.agryvet.malat_api.example.controller
 
 import com.agryvet.malat_api.example.model.HelloWorldBean
 import org.springframework.context.MessageSource
-import org.springframework.context.i18n.LocaleContext
 import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
