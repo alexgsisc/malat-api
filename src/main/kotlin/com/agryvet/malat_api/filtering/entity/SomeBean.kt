37b384dd4cfad79f3acca40f5a9065c68da79536
Alexis Guadarrama <alexgsisc@gmail.com>
2024-09-05 12:54:59 -0600

    :sparkles: I implement @JsonIgnoreProperties,it helps us ignore value static

--- src/main/kotlin/com/agryvet/malat_api/filtering/entity/SomeBean.kt
+++ src/main/kotlin/com/agryvet/malat_api/filtering/entity/SomeBean.kt
@@ -1,7 +1,9 @@
 package com.agryvet.malat_api.filtering.entity
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
+@JsonIgnoreProperties("field1")
 data class SomeBean(
     val field1: String,
     //Annotation for filter static
